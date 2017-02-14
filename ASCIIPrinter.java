80b31405438c07b655cb163f3ef1735f73bffc00
lgsainz <lgsainz15@gmail.com>
2017-02-14 17:31:29 -0500

    folder renaming

---
+++ ASCIIPrinter.java
@@ -0,0 +1,73 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ * This program opens a binary file and prints
+ * all ASCII characters from that file (all bytes
+ * with values between 32 and 126).
+ * 
+ * @author Leticia Garcia-Sainz
+ * @version 11-7-2015
+ */
+
+public class ASCIIPrinter {
+
+	public static void main(String[] args) {
+		
+		// Ask user to input file name
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the filename: ");
+		String file = in.next();
+		
+		// Initialize variables and input/output stream
+		InputStream inStream = null;
+		PrintStream out = null;
+		// Reads the data from the byte
+		int data = 0;
+		// Keeps track of byte data count
+		int count = 0;
+		
+		try {
+			inStream = new FileInputStream(file);
+			out = new PrintStream(new FileOutputStream("ascii.txt"));
+			
+			// Read file
+			while ((data = inStream.read()) != -1) {
+				
+				// Print ASCII characters
+				if (data >= 32 && data <= 126) {
+					System.out.print((char) data);
+					
+					out.print((char) data);
+					count++;
+				}
+				// Print new line after 64 characters
+				if (count % 64 == 0) {
+					System.out.println();
+					count = 0;
+				}
+			}
+		} catch (FileNotFoundException notFound) {
+			notFound.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+            if(inStream != null){
+                try {
+                	// Close InputStream
+                    inStream.close();
+                } catch (IOException ex) {
+                	ex.printStackTrace();	
+                }
+            }
+            // Close PrintStream
+            out.close();
+		}
+	}
+}	
---
+++ PalindromeCheck/ArrayStack.java
@@ -0,0 +1,73 @@
+/**
+ * This class is an implementation of a stack as an array.
+ * Used to implement PalindromeChecker class.
+ * @author Leticia Garcia-Sainz
+ * @version 10-7-2015
+ */
+import java.util.Arrays;
+
+public class ArrayStack {
+
+	private Character stack[];
+	private int currentSize;
+	
+	/**
+	 * Constructs a new stack.
+	 * @param size the size of the stack
+	 */
+	public ArrayStack(int size) {
+		stack = new Character[size];
+		currentSize = 0;
+	}
+	
+	/**
+	 * Returns the current size of the stack (how full it is).
+	 * @return currentSize the size of the stack
+	 */
+	public int size() {
+		return currentSize;
+	}
+	
+	/**
+	 * Adds an element to the top(end) of the stack.
+	 * @param str the element to be added
+	 */
+	public void push(Character str) {
+		if (currentSize == stack.length) {
+			increaseStackSize();
+		}
+		
+		stack[currentSize] = str;
+		currentSize++;
+	}
+	
+	/**
+	 * Removes the element from the end(top) of stack. 
+	 * @return the removed element
+	 */
+	public Character pop() {
+		if (empty()) {
+			System.out.print("The stack is empty");
+		}
+		else {
+			currentSize--;
+		}
+		return stack[currentSize];
+	}
+	
+	/**
+	 * Increases the size of the stack by 1.
+	 */
+	public void increaseStackSize() {
+		stack = Arrays.copyOf(stack, currentSize + 1);
+	}
+	
+	/**
+	 * Checks whether the stack is empty.
+	 * @return true if the stack is empty
+	 */
+	public boolean empty() {
+		return currentSize == 0;
+	}
+	
+}
